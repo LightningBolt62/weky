5abfdc27e8e02ab0a244df95624b0696e2aa2b62
LightningBolt62 <53604347+LightningBolt62@users.noreply.github.com>
2021-09-16 13:05:37 +0600

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,6 @@
+const config = require('../config.json');
 module.exports = async (client, guild) => {
-	await client.channels.cache.get('835142227545686086').send(
+	await client.channels.cache.get(config.guildcreate).send(
 		'```md\n# Guild\n' + guild.name +
 		'\n# Members\n' + guild.memberCount + '\n> ' +
 		client.guilds.cache.size + ' servers```');
