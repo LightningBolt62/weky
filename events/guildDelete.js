f15aaee464999a51ac1cf0bde865b6d691a2b51f
LightningBolt62 <53604347+LightningBolt62@users.noreply.github.com>
2021-09-16 13:07:34 +0600

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,7 @@
+const config = require('../config.json');
 module.exports = async (client, guild) => {
 	if(guild.name == undefined) return;
-	await client.channels.cache.get('835142255793930251').send('```cs\n# Guild\n' + guild.name +
+	await client.channels.cache.get(config.guilddelete).send('```cs\n# Guild\n' + guild.name +
 	'\n# Members\n' + guild.memberCount + '\n> ' +
 	client.guilds.cache.size + ' servers```');
 };
